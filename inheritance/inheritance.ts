8e81c07a727df3eff1649e641d4ba7d942829e27
Tien <tien@Tiens-MacBook-Pro.local>
2024-05-29 23:01:53 +0700

    add inheritance file

---
+++ inheritance/inheritance.ts
@@ -0,0 +1,14 @@
+class Person2 {
+    firstName: string;
+    lastName: string;
+    constructor( firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}.`;
+    }
+}
